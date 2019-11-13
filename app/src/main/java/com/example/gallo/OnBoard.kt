edb3c7ffc31044126a478f67b07227479f54ba35
mnaufalmuafa <naufalmuafa77@gmail.com>
2019-11-13 13:40:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gallo/OnBoard.kt
@@ -0,0 +1,12 @@
+package com.example.gallo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_board)
+    }
+}
